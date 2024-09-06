9b3a5d81834b3244c7fb05165d1cf5f89fb5e27c
Rilr20 <nrrl99@gmail.com>
2024-09-06 21:29:26 +0200

    read file function

--- rust-version/src/main.rs
+++ rust-version/src/main.rs
@@ -1,8 +1,21 @@
+use std::fs;
+use soup;
+
+
 fn main() {
-    println!("Hello, world!");
+    // println!("Hello, world!");
+    let path:&str = "../data/ratings.csv";
+    let mut file_content: String = read_file(path);
+    let mut split_lines = file_content.lines();
+
+    // println!("{:?}", split_lines)
 }
 
-fn read_file() {}
+fn read_file(path: &str) -> String {
+    let content = fs::read_to_string(path)
+        .expect("read content");
+    return content;
+}
 
 fn csv_to_json() {}
 
