3e6a6268f705abaabd8af735ffe8d00c49d88db9
Rilr20 <nrrl99@gmail.com>
2024-09-06 18:12:43 +0200

    rust version

---
+++ rust-version/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+fn read_file() {}
+
+fn csv_to_json() {}
+
+fn scrape_additional_data() {}
+
+fn score_per_director() {}
+
+fn score_per_actor() {}
+
+fn movie_language() {}
+
+fn get_year_data() {}
+
+fn convert_html_to_text() {}
