cd1108efa262ac9d2d7035b6086f3ae72cafa9e5
Rilr20 <nrrl99@gmail.com>
2024-09-08 14:40:11 +0200

    scrapes data from letterboxd

--- rust-version/src/main.rs
+++ rust-version/src/main.rs
@@ -1,23 +1,44 @@
-// use soup;
+use std::collections::HashMap;
 use std::fs::{self, File};
 use std::io::{BufWriter, Write};
 use serde_json::{Value, Map};
-
+use reqwest::{self, Url};
+use std::error::Error;
+use scraper;
 
 fn write_to_file(data:&Vec<Map<String,Value>>) -> std::io::Result<()> {
-    let file = File::create("./rating.json")?;
+    let file = File::create("../data/rating.json")?;
     let mut writer = BufWriter::new(file);
     serde_json::to_writer_pretty(&mut writer, &data)?;
     writer.flush()?;
     Ok(())
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let path: &str = "../data/ratings.csv";
     let content: String = read_file(path);
-    let mut data = csv_to_json(content);
+    let data = csv_to_json(content);
 
     let _ = write_to_file(&data);
+    
+    let mut actors: HashMap<String, Vec<f32>> = HashMap::new();
+    let mut language: HashMap<String, i32> = HashMap::new();
+    let mut directors: HashMap<String, Vec<f32>> = HashMap::new();
+
+
+    let mut idx= 1;
+    for item in data {
+        println!("{}", idx);
+        let _ = scrape_additional_data(item["LetterboxdUri"].to_string(), item["Rating"].to_string(), &mut actors, &mut language, &mut directors).await;
+        idx+=1;
+    }
+
+    println!("{:?}", language);
+    println!("{:?}",directors);
+    println!("{:?}",actors);
+
+
 }
 
 fn read_file(path: &str) -> String {
@@ -65,13 +86,64 @@ fn csv_to_json(data: String) -> Vec<Map<String,Value>> {
     return item_list;
 }
 
-fn scrape_additional_data() {}
+async fn scrape_additional_data(url: String, score: String, actors:&mut HashMap<String, Vec<f32>>, language:&mut HashMap<String, i32>, directors:&mut HashMap<String, Vec<f32>>) -> Result<(), Box<dyn Error>> {
+    let url_ending:Vec<&str> = url.split("/").collect();
+    let base = Url::parse("https://boxd.it/")?;
+    let full_url = format!("{}{}", "https://boxd.it/", url_ending[url_ending.len()-1]);
+    let url = base.join(full_url.split("\"").collect::<Vec<&str>>()[0])?;
 
-fn score_per_director() {}
+    
+    let body = reqwest::get(url)
+        .await?
+        .text()
+        .await?;
+    let document = scraper::Html::parse_document(&body);
 
-fn score_per_actor() {}
+    let html_selector = scraper::Selector::parse("a.text-slug").unwrap();
+    let html_casts = document.select(&html_selector);
 
-fn movie_language() {}
+    let mut no_duplicates: Vec<String> = Vec::new();
+
+    for item in html_casts {
+
+        let mut attr = "";
+        if item.value().attr("href") != None {
+            attr = item.value().attr("href").unwrap();
+            let inner_html = item.text().collect::<String>();
+            let html_text = &inner_html;
+            let split_string: Vec<&str> = attr.split("/").collect();
+            let split_score: Vec<&str> = score.split("\"").collect();
+            let new_score: &str = split_score[1];
+            let score_parsed: f32 = new_score.parse().expect("Failed to parse score");
+
+
+            match split_string[1] {
+                "actor"=> {
+                    // println!("{}", inner_html);
+                    actors.entry(inner_html.to_string()).or_insert(Vec::new()).push(score_parsed);
+                },
+                "director"=> {
+                    // println!("{}", inner_html);
+                    directors.entry(inner_html.to_string()).or_insert(Vec::new()).push(score_parsed);
+                },
+                "films"=> match split_string[2] {
+                    "language" => {
+                        // println!("{}", inner_html);
+                        if !no_duplicates.contains(html_text) {
+                            no_duplicates.push(inner_html.clone());
+                            language.entry(inner_html.to_string()).and_modify(|count| *count += 1).or_insert(1);
+                        }
+                    },
+                    _=> (),
+                    },
+                _ => (),
+            };
+        }
+    }
+
+    Ok(())
+}
+
 
 fn get_year_data() {}
 
